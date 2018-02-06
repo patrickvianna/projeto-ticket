650c9a14e77ab7f7bb7dda1e97d8f1f4159a9e1d
patrick.vianna <patrick.vianna@pentago.com.br>
2018-02-06 12:54:36 -0200

    Validação: Quando não há projetos associados para aquele usuário.

--- angularJS/app/commo/controller/ticket.controller.js
+++ angularJS/app/commo/controller/ticket.controller.js
@@ -9,9 +9,9 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
     }
 
     const vr=  {
-        idUser: JSON.parse(localStorage.getItem(consts.userKey)), 
-        init: '', 
-        max: 10, 
+        idUser: JSON.parse(localStorage.getItem(consts.userKey)),
+        init: '',
+        max: 10,
         project : ''
     }
 
@@ -24,13 +24,13 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
         atualPage : '',
         totalItens : '',
         totalPages : '',
-        startPage: '',							
-        endPage: '',								
-        startIndex: '',							
-        endIndex: '',								
+        startPage: '',
+        endPage: '',
+        startIndex: '',
+        endIndex: '',
         pages: ''
     }
-    
+
 
     /*vm.testeModal = (idTicket) => {
        vm.modalInstance = $uibModal.open({
@@ -42,18 +42,18 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
         controllerAs: '$ctrl',
         size: 'lg',
         resolve: {
-            //parametro: () => 25 
+            //parametro: () => 25
             idTicket: () => idTicket
-        
+
         }
         })
-        
-        
+
+
     } */
-    
+
 
     vm.detailTicket = (id) => {
-        
+
         $uibModal.open({
             template: '<detail-modal tarefa="$ctrl.tarefa" $close="$close(result)" $dismiss="$dismiss(reason)"></detail-modal>',
             controller: [ function() {
@@ -77,11 +77,11 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
         }, function(reason) {
             console.info("I was dimissed, so do what I need to do myContent's controller now and reason was->"+reason);
         });
-        
+
     }
 
     vm.newTicket = () => {
-        
+
         $uibModal.open({
             template: '<new-modal projetos="$ctrl.projetos" $close="$close(result)" $dismiss="$dismiss(reason)"></new-modal>',
             controller: ['projetos', function(projetos) {
@@ -89,7 +89,7 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
                 $ctrl.projetos = projetos
                 //$ctrl.proj.selectedOption.id = ''
                 //$ctrl.proj.selectedOption.name = ''
-               
+
             }],
             controllerAs: '$ctrl',
             size: 'lg',
@@ -105,7 +105,7 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
             //console.info("I was dimissed, so do what I need to do myContent's controller now and reason was->"+reason);
             vm.getTicketProject()
         });
-        
+
     }
 
     function getTickets() {
@@ -129,7 +129,13 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
             .then(resp => {
                 vm.projetos.lista = resp.data
                 vm.projetos.selectedOption = resp.data[0] || ''
-                resolve()
+                if(vm.projetos.lista.length < 1)
+                {
+                  Msg.addError('Não há projetos associados a esse usuário')
+                  reject()
+                }
+                else
+                  resolve()
             }).catch(resp => {
                 Msg.addError('Não foi possível carregar os projetos')
             })
@@ -139,7 +145,7 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
     vm.getTicketProject = (atualPage = 1) => {
         vr.project = vm.projetos.selectedOption.id
         vr.init = atualPage
-        
+
         $http.post(`${consts.apiUrl}/getTicketProject`, vr)
         .then(resp => {
             vm.tickets.tarefas = resp.data.issues
@@ -155,7 +161,7 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
         // calculate total pages
         let totalPages = Math.ceil(totalItens / pageSize);				//CALCULO DA QUANTIDADE DE PAGINAS
         let startPage, endPage;												//START PRIMEIRA E ULTIMA PAGINAS
-        
+
         if (totalPages <= 4) {											    //SE TIVER MENOS QUE 10 PAGINAS MOSTRE TUDO
             startPage = 1;													//A PRIMEIRA PAGINA VAI SER 1
             endPage = totalPages;											//A ULTIMA VAI SER O NUMERO DO TOTAL
@@ -163,19 +169,19 @@ function TicketController($scope, $http, $location, consts, $state, Msg, $q, $ro
             if (atualPage <= 3) {										///SE A PAGINA ATUAL FOR MENOR OU IGUAL A 6
                 startPage = 1;													//A PRIMEIRA PAGINA RECEBE 1
                 endPage = 4;													//A ULTIMA PAGINA RECEBE 10
-            }    
+            }
             else if (atualPage+ 1 >= totalPages) {						    //SE A (PAGINA ATUAL + 4) FOR MAIOR OU IGUAL AO TOTAL DE PAGINAS
                 startPage = totalPages - 3;										//A PRIMEIRA PAGINA RECEBE O TOTAL DE PAGINAS MENOS 9
                 endPage = totalPages;											//A ULTIMA PAGINA RECEBE O TOTAL DE PAGINAS
-            } else {                                                            //SE NAO    
+            } else {                                                            //SE NAO
                 startPage = atualPage;							    //A PRIMEIRA PAGINA RECEBE A PAGINA ATUAL MENOS 5
                 endPage = atualPage + 1;									//A ULTIMA PAGINA RECEBE A PAGINA ATULA MAIS 4
-            }																	
+            }
         }
         // calculate start and end item indexes								    --CALCULANDO O ITEM DO COMEÇO E DO FINAL
         let startIndex = (atualPage - 1) * pageSize;					    //O PRIMEIRO INDEX RECEBE A (PAGINA ATUAL -1) VEZES O TAMANHO DA PAGINA
         let endIndex = Math.min(startIndex + pageSize - 1, totalItens - 1);	//O ULTIMO INDEX RECEBE O MENOR VALOR DENTRE (O PRIMEIRO INDEX + TAMANHO DA PAGINA -1, OU TOTAL DE ITENS -1)
-    
+
         // create an array of pages to ng-repeat in the pager control		    --CRIA UM ARRAY DE PAGINAS PARA O NG-REPEAT NO CONTROLLER
         let pages = [];
         for(let i = startPage; i <= endPage; i++)                               //FOR PARA PEGAR TODAS AS PAGINAS, DO INICIO AO FIM
